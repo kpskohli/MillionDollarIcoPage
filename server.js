b6a54c11d08f496021b0004cbd549ecbd8ea4e04
Sameep Singhania <sameepsi@gmail.com>
2017-12-23 00:59:03 +0530

    admin changes and right

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
-app.use(serveStatic(__dirname + "/build"));
+app.use('/', express.static(__dirname + '/build'));
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
